d82682de17352634f312e53a4c0b8f1c2861b35d
kcho760 <kcho760@gmail.com>
2023-03-24 14:39:19 -0400

    Added init and parent=

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,24 @@
 class PolyTreeNode
-  
+
+    attr_reader :value,:parent, :children
+
+    def initialize(value)
+        @parent = nil
+        @value = value
+        @children = []
+    end
+
+    def parent=(new_parent)
+        if new_parent == nil && @parent != nil
+            @parent.children.delete(self)
+        elsif
+            new_parent != nil && @parent != nil
+            @parent.children.delete(self)
+            new_parent.children << self if !new_parent.children.include?(self)
+        elsif new_parent != nil && @parent == nil
+            new_parent.children << self if !new_parent.children.include?(self)
+        end
+        @parent = new_parent
+    end
+
 end
