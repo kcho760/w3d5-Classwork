27bc74c4afa6b525e1edf6c2ce4c535079c7e905
kcho760 <kcho760@gmail.com>
2023-03-27 15:36:18 -0400

    Children added

--- practice-for-ch-ruby-tic-tac-toe-ai-long-practice-main/lib/tic_tac_toe_node.rb
+++ practice-for-ch-ruby-tic-tac-toe-ai-long-practice-main/lib/tic_tac_toe_node.rb
@@ -2,6 +2,22 @@ require_relative 'tic_tac_toe'
 
 class TicTacToeNode
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos= prev_move_pos
+  end
+
+  def children
+
+    pot_positions = @board.open_positions
+    pot_positions.each do |pot_position|
+      current_board = @board.dup
+      current_board.place_mark(pot_position,next_mover_mark)
+      switched_mark = current_board.next_mark
+      TicTacToeNode.new(current_board,switched_mark,pot_position)
+    end
+    
+
   end
 
   def losing_node?(evaluator)
